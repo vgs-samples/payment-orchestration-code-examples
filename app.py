5436dcb7ca7b20e528bf308257ed6bd594e54c50
flor-master <Alexandr.Florinskij@yandex.ua>
2022-01-26 18:36:56 +0200

    last changes

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import Flask
 from flask import render_template, request
 from flask import request
 from flask_cors import CORS
+from pathlib import Path
 
 app = Flask(__name__)
 auth_api = "https://auth.verygoodsecurity.com/auth/realms/vgs/protocol/openid-connect/token"
 
 VAULT_ID = os.environ.get('PAYMENT_ORCH_VAULT_ID')
+CUSTOMER_VAULT_ID = os.environ.get('CUSTOMER_VAULT_ID')
+PAYMENT_ORCH_OUTBOUND_PROXY_ID = os.environ.get('PAYMENT_ORCH_OUTBOUND_PROXY_ID')
+PAYMENT_ORCH_OUTBOUND_PROXY_SECRET = os.environ.get('PAYMENT_ORCH_OUTBOUND_PROXY_SECRET')
 
 CORS(app)
 
@@ -24,20 +28,31 @@ def get_access_token():
         
 @app.route("/")
 def index():
-    return render_template('./index.html', customerVaultId = os.environ.get('CUSTOMER_VAULT_ID'))
+    print(Path(__file__).resolve().parent / f'certs/sandbox_cert.pem')
+
+    return render_template('./index.html', customerVaultId = CUSTOMER_VAULT_ID)
 
 @app.route("/checkout", methods=['POST'])
 def checkout():
     # ==> Save aliassed credit card to DB here <==
     access_token = get_access_token()
+    proxies = {
+        'https': 'https://{}:{}@{}.sandbox.verygoodproxy.com:8443'.format(PAYMENT_ORCH_OUTBOUND_PROXY_ID, PAYMENT_ORCH_OUTBOUND_PROXY_SECRET, CUSTOMER_VAULT_ID),
+    }
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer {}".format(access_token['access_token'])
     }
     fin_instr_data = request.get_json()
+    
+    print(Path(__file__).resolve().parent / f'certs/sandbox_cert.pem' )
     fin_instr = requests.post('https://{}.sandbox.verygoodproxy.com/financial_instruments'.format(VAULT_ID),
         headers = headers,
-        json = fin_instr_data
+        json = fin_instr_data,
+        proxies = proxies,
+        # verify = '/Users/flor/workspace/work/payment-orchestration-demo/certs/sandbox_cert.pem'
+        verify = Path(__file__).resolve().parent / f'certs/sandbox_cert.pem'
+        # verify = False
     )
     
     transfers_data = {
@@ -45,9 +60,15 @@ def checkout():
         "currency": "USD",
         "source": fin_instr.json()['data']['id'],
     }
-    tansfer = requests.post('https://{}.sandbox.verygoodproxy.com/transfers'.format(VAULT_ID),
+    
+    tansfer = requests.post(
+        'https://{}.sandbox.verygoodproxy.com/transfers'.format(VAULT_ID),
         headers = headers,
-        json = transfers_data
+        json = transfers_data,
+        proxies = proxies,
+        verify = Path(__file__).resolve().parent / f'certs/sandbox_cert.pem'
+        # verify = False
     )
+    print(tansfer.json())
     return tansfer.json()
 
