69e475a7bb0a9efbeb7540f1a501056aa4542127
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-14 18:08:19 -0700

    Fix the demo for making request

--- ruby-auth/demo.rb
+++ ruby-auth/demo.rb
@@ -16,16 +16,18 @@ access_token = client.client_credentials.get_token
 # Here's your token
 puts access_token.token
 
-con = Faraday.new 'https://tntsfeqzp4a.sandbox.verygoodproxy.com'
 # Then, for example, say you want to create a stripe gateway,
 # with the access token given if it has enough permission,
-# you can do this
+# you can do something like this:
+con = Faraday.new 'https://tntsfeqzp4a.sandbox.verygoodproxy.com'
 res = con.post do |req| 
   req.url '/gateways' 
   # Here you pass the access token value by setting the `Authorization` header
   # with value as "Bearer #{access_token.token}"
-  req.headers['Authorization'] = "Bearer #{access_token.token}"
-  {
+  # req.headers['Authorization'] = "Bearer #{access_token.token}"
+  req.headers['X-Auth-Token'] = token
+  req.headers['Content-Type'] = 'application/json'
+  req.body = {
     id: "stripe-default",
     type: "stripe",
     default_gateway: true,
@@ -33,6 +35,6 @@ res = con.post do |req|
     config: {
       login: "<STRIPE_API_KEY>"
     }
-  }
+  }.to_json
 end
 puts res.inspect
