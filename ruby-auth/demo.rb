7a8166146bd42fb464a667d4fffc4d146b5b648b
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-14 18:00:09 -0700

    More example

--- ruby-auth/demo.rb
+++ ruby-auth/demo.rb
@@ -9,5 +9,28 @@ client = OAuth2::Client.new(
   token_url: token_url,
 )
 
-token = client.client_credentials.get_token
-puts token
+# This creates a token for accessing VGS payment orchestration API
+access_token = client.client_credentials.get_token
+
+# Here's your token
+puts access_token.token
+
+# Then, for example, say you want to create a stripe gateway,
+# with the access token given if it has enough permission,
+# you can do this
+res = con.post do |req| 
+  req.url 'https://tntsfeqzp4a.sandbox.verygoodproxy.com/gateways' 
+  # Here you pass the access token value by setting the `Authorization` header
+  # with value as "Bearer #{access_token.token}"
+  req.headers['Authorization'] = "Bearer #{access_token.token}"
+  {
+    id: "stripe-default",
+    type: "stripe",
+    default_gateway: true,
+    default_currency: "USD",
+    config: {
+      login: "<STRIPE_API_KEY>"
+    }
+  }
+end
+puts res.inspect
