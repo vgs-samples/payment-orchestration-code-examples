c9eae0131e6aca88e9b926995cbacf0ee74fecfd
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-14 17:39:02 -0700

    Make token url coming from env var

--- ruby-auth/demo.rb
+++ ruby-auth/demo.rb
@@ -2,10 +2,11 @@ require 'oauth2'
 
 client_id = ENV["VGS_CLIENT_ID"]
 client_secret = ENV["VGS_CLIENT_SECRET"]
+token_url = ENV["VGS_OAUTH_TOKEN_ENDPOINT"] || "https://auth.verygoodsecurity.com/auth/realms/vgs/protocol/openid-connect/token"
 client = OAuth2::Client.new(
   client_id,
   client_secret,
-  token_url: 'https://auth.verygoodsecurity.com/auth/realms/vgs/protocol/openid-connect/token'
+  token_url: token_url,
 )
 
 token = client.client_credentials.get_token
