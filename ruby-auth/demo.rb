14e3661391f93c7f5f973ec698a0f0f64f2fad3e
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-14 17:34:54 -0700

    Code style

--- ruby-auth/demo.rb
+++ ruby-auth/demo.rb
@@ -2,7 +2,11 @@ require 'oauth2'
 
 client_id = ENV["VGS_CLIENT_ID"]
 client_secret = ENV["VGS_CLIENT_SECRET"]
-client = OAuth2::Client.new(client_id, client_secret, token_url: 'https://auth.verygoodsecurity.com/auth/realms/vgs/protocol/openid-connect/token')
+client = OAuth2::Client.new(
+  client_id,
+  client_secret,
+  token_url: 'https://auth.verygoodsecurity.com/auth/realms/vgs/protocol/openid-connect/token'
+)
 
 token = client.client_credentials.get_token
 puts token
