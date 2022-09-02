4033ed436c55c2e839323da12391778afe7bdd04
Alexandr <alexandr.florinskiy@vgs.io>
2022-09-02 17:05:58 +0300

    add 3ds flow

--- static/three_ds.js
+++ static/three_ds.js
@@ -1,142 +1,11 @@
 const amount = 1000;
 const currency = "USD";
-const vgsUrl =
-  "https://tntipgdjdyl-4880868f-d88b-4333-ab70-d9deecdbffc4.sandbox.verygoodproxy.com";
+const vgsUrl = "https://tntmrn77zaf-4880868f-d88b-4333-ab70-d9deecdbffc4.sandbox.verygoodproxy.com";
 const readAccessToken = () => document.getElementById("token").textContent;
 
-const makeTransferWith3DS = (fId, threeDsData = false) => {
-  console.log('TRANSFER', fId, threeDsData )
-  let body = {
-    amount: amount,
-    currency: currency,
-    source: fId, 
-  }
-  if (threeDsData) {
-    body["three_ds_authentication"] = threeDsData.data.id
-  } 
-
-  return fetch(`${vgsUrl}/transfers`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${readAccessToken()}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      checkout.update({
-        formStatus: "success"
-      });
-    });
-};
-
-const tryDeviceFingerprint = (data) => {
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute("name", "fingerprint");
-  document.body.appendChild(iframe);
-
-  const form = document.createElement("form");
-  form.setAttribute("action", data.data.device_fingerprint.url);
-  form.setAttribute("target", "fingerprint");
-  form.setAttribute("method", "POST");
-  for (const [key, value] of Object.entries(
-    data.data.device_fingerprint.params
-  )) {
-    const input = document.createElement("input");
-    input.setAttribute("name", key);
-    input.setAttribute("value", value);
-    form.appendChild(input);
-  }
-  document.body.appendChild(form);
-  form.submit();
-
-  return new Promise((resolve, reject) => {
-    let callback = {};
-    callback.value = (message) => {
-      console.log("fingerprint message from iframe", message.data);
-      window.removeEventListener("message", callback.value);
-
-      resolve(
-        fetch(
-          `${vgsUrl}/threeds_authentications/${data.data.id}/fingerprints`,
-          {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${readAccessToken()}`,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              complete_indicator: "Y",
-            }),
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("complete fingerprints", data);
-            return data;
-          })
-      );
-    };
-    window.addEventListener("message", callback.value);
-  });
-};
-
-const tryChallengeFlow = (data) => {
-  console.log("Challenge Flow ==>", data);
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute("name", "challenge");
-  iframe.src = `${data.data.challenge.url}`;
-  iframe.width = 600;
-  iframe.height = 700;
-  document.body.appendChild(iframe);
-
-  const form = document.createElement("form");
-  form.setAttribute("action", data.data.challenge.url);
-  form.setAttribute("target", "challenge");
-  form.setAttribute("method", "POST");
-  for (const [key, value] of Object.entries(data.data.challenge.params)) {
-    const input = document.createElement("input");
-    input.setAttribute("name", key);
-    input.setAttribute("value", value);
-    form.appendChild(input);
-  }
-  document.body.appendChild(form);
-  form.submit();
-
-  return new Promise((resolve, reject) => {
-    let callback = {};
-    callback.value = (message) => {
-      console.log("challenge message from iframe", message.data);
-      window.removeEventListener("message", callback.value);
-      resolve(
-        fetch(`${vgsUrl}/threeds_authentications/${data.data.id}/challenges`, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${readAccessToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            trans_status: "Y",
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("complete challenge", data);
-            return data;
-          })
-      );
-    };
-    window.addEventListener("message", callback.value);
-  });
-};
-
-const threeDsAuth = (fId) => {
-  // const fId = document.getElementById("fin_id_input").value;
-
+const authentication = async (finInstrumentId) => {
   const data3ds = {
-    card: fId,
+    card: finInstrumentId,
     amount: amount,
     currency: currency,
     origin: window.location.origin,
@@ -150,7 +19,7 @@ const threeDsAuth = (fId) => {
       user_agent: window.navigator.userAgent,
     },
   };
-  fetch(`${vgsUrl}/threeds_authentications`, {
+  const authRespRaw = await fetch(`${vgsUrl}/threeds_authentications`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${readAccessToken()}`,
@@ -158,40 +27,123 @@ const threeDsAuth = (fId) => {
     },
     body: JSON.stringify(data3ds),
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("3ds_authentications ==>", data.data);
-      if (data.data.state === "device_fingerprint") {
-        return tryDeviceFingerprint(data);
-      }
-      if (data.data.state === "challenge") {
-        return tryChallengeFlow(data);
-      }
-      if (data.data.state === "successful") {
-        return makeTransferWith3DS(fId, data);
-      }
-      if (data.data.error_code === "AuthenticationNotRequired") {
-        return makeTransferWith3DS(fId);
-      }
-    })
-    .then((data) => {
-      console.log("3ds_authentications 2 ==>", data.data);
-      if (data.data.state === "device_fingerprint") {
-        return tryDeviceFingerprint(data);
-      }
-      if (data.data.state === "challenge") {
-        return tryChallengeFlow(data);
-      }
-      if (data.data.state === "successful") {
-        return makeTransferWith3DS(fId, data);
-      }
-      if (data.data.error_code === "AuthenticationNotRequired") {
-        return makeTransferWith3DS(fId);
-      }
-    });
-};
+  
+  return await authRespRaw.json()
+}
 
-document.getElementById("three_ds_btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  // threeDsAuth();
-});
+const deviceFingerptint = async (fingerprint) => {
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute("name", "fingerprint");
+  iframe.setAttribute("id", "vgs-device-fingerprint-iframe")
+  document.body.appendChild(iframe);
+
+  const form = document.createElement("form");
+  form.setAttribute("action", fingerprint.device_fingerprint.url);
+  form.setAttribute("target", "fingerprint");
+  form.setAttribute("method", "POST");
+  form.setAttribute("id", "vgs-device-fingerprint-form")
+  for (const [key, value] of Object.entries(
+    fingerprint.device_fingerprint.params
+  )) {
+    const input = document.createElement("input");
+    input.setAttribute("name", key);
+    input.setAttribute("value", value);
+    form.appendChild(input);
+  }
+  document.body.appendChild(form);
+  await form.submit();
+  const deviceFingerptintFinishRaw = await fetch( `${vgsUrl}/threeds_authentications/${fingerprint.id}/fingerprints`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${readAccessToken()}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      complete_indicator: "Y",
+    }),
+  })
+  const deviceFingerptintFinish = await deviceFingerptintFinishRaw.json()
+  document.getElementById('vgs-device-fingerprint-iframe').remove()
+  document.getElementById('vgs-device-fingerprint-form').remove()
+  return deviceFingerptintFinish
+}
+
+const tryChallenge = async (challenge) => {
+  console.log('Start Challenge', challenge)
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute("name", "challenge");
+  iframe.setAttribute("id", "vgs-device-challenge-iframe")
+  iframe.width = 600;
+  iframe.height = 700;
+  document.body.appendChild(iframe);
+
+  const form = document.createElement("form");
+  form.setAttribute("action",challenge.challenge.url);
+  form.setAttribute("target", "challenge");
+  form.setAttribute("method", "POST");
+  form.setAttribute("id", "vgs-device-challenge-form")
+
+  for (const [key, value] of Object.entries(
+    challenge.challenge.params
+  )) {
+    const input = document.createElement("input");
+    input.setAttribute("name", key);
+    input.setAttribute("value", value);
+    form.appendChild(input);
+  }
+  document.body.appendChild(form);
+  console.log('submit')
+  await form.submit();
+  console.log('submit Finish')
+  window.addEventListener("message", (data) => {
+    console.log('data', data)
+  });
+  const challengeFinishRaw = await fetch(`${vgsUrl}/threeds_authentications/${challenge.id}/challenges`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${readAccessToken()}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      trans_status: "Y",
+    }),
+  })
+  const challengeFinish = challengeFinishRaw.json()
+  console.log('challenge finish -->', challengeFinish)
+  document.getElementById('vgs-device-challenge-iframe').remove()
+  document.getElementById('vgs-device-challenge-form').remove()
+  return challengeFinish
+}
+
+
+const three = async (fId) => {
+  const auth = await authentication(fId)
+  console.log('==> Before make3DS()')
+  const threeDS = await make3DS(auth.data)
+  console.log('==> LAST RETURN!', threeDS)
+  return threeDS
+  async function make3DS(threeDSData) {
+    console.log('==> make3DS() start')
+    let threeDSResult = null
+    let all = null
+    switch (threeDSData.state) {
+      case "device_fingerprint":
+        console.log('==> Device Fingerprint Case')
+        threeDSResult = await deviceFingerptint(threeDSData)
+        all = await make3DS(threeDSResult.data)
+        return all
+      case "challenge":
+        console.log('==> Challenge Case')
+        threeDSResult = await tryChallenge(threeDSData)
+        console.log('==> Challenge Finish',  threeDSResult)
+        all = await make3DS(threeDSResult.data)
+        return all
+      case "successful":
+        console.log('==> 3ds Successful state', threeDSData)
+        return threeDSData
+      default:
+        console.log('==> Unexpected responce', threeDSData)
+        return threeDSData
+    }
+  }
+}
